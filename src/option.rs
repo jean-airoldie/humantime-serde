8dcb38a13b412c472a09cfb701cd20a3ba7acdf2
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-03-11 17:11:32 +0000

    option: Adjust docs and add example

--- src/option.rs
+++ src/option.rs
@@ -1,9 +1,28 @@
-//! Module to adaopt `humantime_serde` to `Option<Duration>`
+//! Module to adaopt `humantime_serde` to `Option`
+//!
+//! # Example
+//!
+//! ```
+//! use serde::{Serialize, Deserialize};
+//! use std::time::{Duration, SystemTime};
+//!
+//! #[derive(Serialize, Deserialize)]
+//! struct Foo {
+//!     #[serde(with = "humantime_serde::option")]
+//!     timeout: Option<Duration>,
+//!     #[serde(default)]
+//!     #[serde(with = "humantime_serde::option")]
+//!     time: Option<SystemTime>,
+//! }
+//! ```
 
 use super::Serde;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
-/// Serializes an `Option<Duration>` or `Option<SystemTime>` via the humantime crate.
+/// Serializes an `Option<Duration>` or `Option<SystemTime>`
+///
+/// This function can be used with `serde_derive`'s `with` and
+/// `deserialize_with` annotations.
 pub fn serialize<T, S>(d: &Option<T>, s: S) -> Result<S::Ok, S::Error>
 where
     for<'a> Serde<&'a T>: Serialize,
@@ -13,7 +32,10 @@ where
     nested.serialize(s)
 }
 
-/// Deserialize an `Option<Duration>` or `Option<SystemTime>` via the humantime crate.
+/// Deserialize an `Option<Duration>` or `Option<SystemTime>`
+///
+/// This function can be used with `serde_derive`'s `with` and
+/// `deserialize_with` annotations.
 pub fn deserialize<'a, T, D>(d: D) -> Result<Option<T>, D::Error>
 where
     Serde<T>: Deserialize<'a>,
